18e4264f7d819ff74f49fb780e48bb83352f581b
kornelc <kornel@netgurus.com>
2021-02-16 13:21:14 -0500

    lecture 6

--- src/main/scala/com/synkre/personapp/PersonStore.scala
+++ src/main/scala/com/synkre/personapp/PersonStore.scala
@@ -6,14 +6,16 @@ import org.json4s.DefaultFormats
 import org.json4s.native.JsonMethods.parse
 import org.json4s.native.Serialization.write
 
+import scala.util.Try
+
 class PersonStore{
   implicit val formats = DefaultFormats
   val path = Paths.get("/tmp/people.json")
-  def storePeople(people: Seq[Person]): Unit ={
+  def storePerson(people: Seq[Person]): Try[Unit] = Try{
     val jsonText = write(people)
     Files.write(path, jsonText.getBytes)
   }
-  def readPeople: Seq[Person] = {
+  def readPerson: Try[Seq[Person]] = Try{
     val readData = Files.readAllBytes(path)
     parse(new String(readData)).extract[Seq[Person]]
   }
--- src/test/scala/com/synkre/personapp/PersonStoreSpec.scala
+++ src/test/scala/com/synkre/personapp/PersonStoreSpec.scala
@@ -4,13 +4,15 @@ import com.synkre.personapp.Person._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.util.Success
+
 class PersonStoreSpec extends AnyFlatSpec with Matchers{
   "PersonStore" should "store and retrieve people from a JSON file" in {
     val store = new PersonStore
     val people = Seq(denzelWashington, markHamill)
-    store.storePeople(people)
-    val people2 = store.readPeople
-    people2 should contain theSameElementsInOrderAs people
+    store.storePerson(people)
+    val Success(people2) = store.readPerson
+    people should contain theSameElementsInOrderAs people2
   }
 }
 
