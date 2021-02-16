fc467954b9c7738e9686396812e2ca5c89a287eb
kornelc <kornel@netgurus.com>
2021-02-16 07:47:17 -0500

    lecture two

--- build.sbt
+++ build.sbt
@@ -4,9 +4,3 @@ version := "0.1"
 
 scalaVersion := "2.13.4"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.2" % "test"
-libraryDependencies += "org.apache.commons" % "commons-text" % "1.9"
-libraryDependencies += "org.json4s" % "json4s-native_2.13" % "3.7.0-M8"
-libraryDependencies += "org.mockito" % "mockito-core" % "2.22.0"
-libraryDependencies += "com.typesafe" % "config" % "1.4.0"
-
--- src/main/scala/com/synkre/personapp/PersonApp.scala
+++ src/main/scala/com/synkre/personapp/PersonApp.scala
@@ -1,12 +1,5 @@
 package com.synkre.personapp
 
-import com.synkre.personapp.Person.{denzelWashington, markHamill}
-
 object PersonApp extends App {
-  val appConf = PersonAppConf.loadConf("config/prod/application.conf")
-  val store = new PersonStore(appConf.personStoreConf)
-  val people = Seq(denzelWashington, markHamill)
-  store.storePerson(people)
-  val people2 = store.readPerson
-  people2.foreach(println)
+  println("Hello, World!")
 }
